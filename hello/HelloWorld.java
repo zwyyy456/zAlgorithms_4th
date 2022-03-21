6c3d2d53215b56060f120c3512d59dc282d1d2b1
zwyyy <zwyyy@zwyyy-S3.lan>
2022-03-21 17:06:39 +0800

    debian

---
+++ hello/HelloWorld.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+package PACKAGE_NAME;
+public class HelloWorld {
+    public static void main(String[] args) {
+        
+    }
+}
