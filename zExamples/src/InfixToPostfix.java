f0806cb3ac2d9ec20cc0df8dca8efb93ab9da781
zwyyy456 <zwyyy456@hotmail.com>
2022-04-18 17:03:36 +0800

    deb

---
+++ zExamples/src/InfixToPostfix.java
@@ -0,0 +1,22 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class InfixToPostfix {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<String>();
+        while (!StdIn.isEmpty()) {
+            String s = StdIn.readString();
+            if (s.equals("+"))
+                stack.push(s);
+            else if (s.equals("*"))
+                stack.push(s);
+            else if (s.equals(")"))
+                StdOut.print(stack.pop() + " ");
+            else if (s.equals("("))
+                StdOut.print("");
+            else
+                StdOut.print(s + " ");
+        }
+        StdOut.println();
+    }
+}
